d74430e0ecdca30c1321ec2feb81bcbe58b8e582
Julia <juliacapodarco@hotmail.com>
2026-03-25 16:00:26 -0300

    Agrego ejercicio1 en src y configuro gitignore

---
+++ src/ejercicio1.py
@@ -0,0 +1,24 @@
+#funciones ejercicio 1
+def obtener_lineas(texto):
+    return texto.splitlines()
+
+def contar_lineas(lineas):
+    return len(lineas)
+
+def contar_palabras(lineas):
+    total = 0
+    for linea in lineas:
+        palabras = linea.split()
+        total += len(palabras)
+    return total
+
+def calcular_promedio(total_palabras, total_lineas):
+    return total_palabras / total_lineas
+
+def lineas_mayores_al_promedio(lineas, promedio):
+    resultado = []
+    for linea in lineas:
+        cantidad = len(linea.split())
+        if cantidad > promedio:
+            resultado.append(linea)
+    return resultado
