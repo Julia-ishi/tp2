b63ed7542179faafc52a192ac2598ccb664b6f6b
Julia <juliacapodarco@hotmail.com>
2026-03-31 16:55:22 -0300

    corrijo ejercicio 6

--- src/ejercicio6.py
+++ src/ejercicio6.py
@@ -1,37 +1,36 @@
 # funciones ejercicio 6
+from collections import Counter
 
 def extraer_hashtags(post):
-    palabras = post.split() # separo el post en palabras
+    palabras = post.split()
     hashtags = []
+
     for palabra in palabras:
-        if palabra.startswith("#"): # si la palabra empieza con # es un hashtag
-            # saco signos como coma o punto si tiene
+        if palabra.startswith("#"):
             hashtag = palabra.strip(".,")
             hashtags.append(hashtag)
-    return hashtags # devuelve una lista con los hashtags encontrados en el post
+    return hashtags
 
 def obtener_todos_los_hashtags(posts):
     todos = []
-    for post in posts:
-        hashtags = extraer_hashtags(post) # extraigo los hashtags de cada post
-        todos.extend(hashtags) # agrego los hashtags a la lista de todos los hashtags
-    return todos # devuelve una lista con todos los hashtags encontrados en todos los posts
 
-def contar_frecuencias(hashtags):
-    frecuencias = {}   # diccionario para contar la frecuencia de cada hashtag
-    for hashtag in hashtags:
-        if hashtag in frecuencias:
-            frecuencias[hashtag] += 1
-        else:
-            frecuencias[hashtag] = 1
-    return frecuencias
+    for post in posts:
+        hashtags = extraer_hashtags(post)
+        todos.extend(hashtags)
+    return todos
+
+
+def contar_frecuencias(posts):
+    hashtags = obtener_todos_los_hashtags(posts)
+    return Counter(hashtags)
 
 def obtener_trending(frecuencias):
     trending = {}
-    for hashtag, cantidad in frecuencias.items(): #recorro el diccionario de frecuencias
+
+    for hashtag, cantidad in frecuencias.items():
         if cantidad > 1:
             trending[hashtag] = cantidad
-    return trending     # devuelve un diccionario con los hashtags que tienen una frecuencia mayor a 1
+    return trending
 
 def total_hashtags_unicos(frecuencias):
-    return len(frecuencias)  # devuelve la cantidad de hashtags únicos
+    return len(frecuencias)
